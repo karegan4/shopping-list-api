53891b1eb542e260b4f379495975aebc2e93c319
Kaitlin Regan <karegan@gmail.com>
2021-06-02 14:25:20 -0400

    Added ability to edit item names

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,7 +29,7 @@ class ItemsController < ApplicationController
     def update
         item = Item.find(params[:id])
         item.update(item_params)
-        render json: Item.new(item)
+        render json: item
     end
 
     private
